939e7562c3abb6d50f0dcbb2f197fad16f123377
lgcastellucci <castellucci@gmail.com>
2024-05-26 15:44:21 -0300

    Testando outra pagina inicial para release

--- Program.cs
+++ Program.cs
@@ -15,6 +15,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseAuthorization();
 
